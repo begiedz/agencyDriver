d340e1ef4227288d0ed95c5c4fe51a12e76f7b1f
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-10-31 18:06:22 +0100

    add dummy data

---
+++ src/data.ts
@@ -0,0 +1,38 @@
+export let rides:Array<object> = [
+  {
+    rideID: 1,
+    date:"31-10-2023",
+    startLocationName: "Agency Hotel",
+    startLocationAdress: "Runderweg 11 1234 AB Amsterdam",
+    distance:20,
+    destinationName: "Jumbo Online",
+    destinationAdress:"Gyroscoopweg 10 1042 AC Amsterdam",
+  },  
+  {
+    rideID: 2,
+    date:"31-10-2023",
+    startLocationName: "Jumbo Online",
+    startLocationAdress:"Gyroscoopweg 10 1042 AC Amsterdam",
+    distance:20,
+    destinationName: "Agency Hotel",
+    destinationAdress: "Runderweg 11 1234 AB Amsterdam",
+  },
+  {
+    rideID: 3,
+    date:"31-10-2023",
+    startLocationName: "Agency Headquaters",
+    startLocationAdress:"Valkhofparkstraat 123 1122 AD Nijmegen",
+    distance:117,
+    destinationName: "Agency Hotel",
+    destinationAdress: "Runderweg 11 1234 AB Amsterdam",
+  },
+  {
+    rideID: 3,
+    date:"31-10-2023",
+    startLocationName: "Agency Hotel",
+    startLocationAdress: "Runderweg 11 1234 AB Amsterdam",
+    distance:117,
+    destinationName: "Agency Headquaters",
+    destinationAdress:"Valkhofparkstraat 123 1122 AD Nijmegen",
+  },
+]
